405e9fe7ee1a1ccc85ecccb3fc933b9955eca5d5
Farzane Nazmabadi <1379farzanenazmabadi@gmail.com>
2024-03-09 15:03:44 +0330

    add persian calender

---
+++ scripts/custom.js
@@ -0,0 +1,29 @@
+// This file is for setting and customizing the calendar
+let option1 = {
+    placeholder: "تاریخ رفت",
+    twodigit: false,
+    closeAfterSelect: true,
+    nextButtonIcon: "images/timeir_next.png",
+    previousButtonIcon: "images/timeir_prev.png",
+    forceFarsiDigits: true,
+    markToday: true,
+    markHolidays: true,
+    highlightSelectedDay: true,
+    sync: false,
+    gotoToday: true
+};
+kamaDatepicker('calender1', option1);
+let option2 = {
+    placeholder: "تاریخ برگشت",
+    twodigit: false,
+    closeAfterSelect: true,
+    nextButtonIcon: "images/timeir_next.png",
+    previousButtonIcon: "images/timeir_prev.png",
+    forceFarsiDigits: true,
+    markToday: true,
+    markHolidays: true,
+    highlightSelectedDay: true,
+    sync: false,
+    gotoToday: true
+};
+kamaDatepicker('calender2', option2);
